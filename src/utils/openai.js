7e1c7e673585fbf4c9818f8d095562dd9fd7595a
Tejas pagare <tejaspagare1625@gmail.com>
2024-09-23 00:53:31 +0530

    updating certain key

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -9,8 +9,8 @@
 // export default openai;
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { GEMINI_API_KEY } from "./constants";
-const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export default model;
